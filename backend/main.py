a6d07db444b47d447d7e703098b9dc6263c7b539
Priya Sridhar <priyasridhar101@gmail.com>
2024-06-19 15:39:12 +0530

    api done

--- backend/main.py
+++ backend/main.py
@@ -24,6 +24,36 @@ def create_contact():
         db.session.commit()
     except Exception as e:
         return (jsonify({"message":str(e)}),500,)
+    
+    return (jsonify({"message":"Contact created"}),201,)
+
+@app.route("/update_contact/<int:user_id>",methods=["PUT"])
+def update_contact(user_id):
+    contact=Contact.query.filter_by(id=user_id)
+
+    if not contact:
+        return jsonify({"message":"Contact not found"}),404
+    
+    data=request.json
+    #if first name exists in the data, update the contact's first name, else keep what u have
+    contact.first_name=data.get("firstName",contact.first_name)
+    contact.last_name=data.get("lastName",contact.last_name)
+    contact.email=data.get("email",contact.email)
+
+    db.session.commit()
+    return (jsonify({"message":"Contact updated"}),200)
+
+@app.route("/delete_contact/<int:user_id>",methods=["DELETE"])
+def delete_contact(user_id):
+    contact=Contact.query.filter_by(id=user_id)
+
+    if not contact:
+        return jsonify({"message":"Contact not found"}),404
+    
+    db.session.delete(contact)
+    db.session.commit()
+
+    return (jsonify({"message":"Contact deleted"}),200)
 
 #only if you run the file directly do this, if import don't execute
 if __name__=="__main__":
