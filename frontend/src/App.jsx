cee582dfd692238eafa672867fa2ea07e9c579a5
Priya Sridhar <priyasridhar101@gmail.com>
2024-06-19 16:47:55 +0530

    styling

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,22 +1,49 @@
 import { useState, useEffect } from 'react'
 import './App.css'
 import ContactList from './ContactList'
+import ContactForm from './ContactForm'
 
 function App() {
-  const [contacts, setContacts]= useState([])
+  const [contacts, setContacts] = useState([])
+  //modal initially closed (false), opens when true
+  const [isModalOpen, setIsModalOpen] = useState(false)
 
   useEffect(() => {
-    //fetchContacts()
+    fetchContacts()
   }, [])
 
   const fetchContacts = async () => {
-    const response =await fetch("http://127.0.0.1:5000/contacts")
+    const response = await fetch("http://127.0.0.1:5000/contacts")
     const data = await response.json()
     setContacts(data.contacts)
     console.log(data.contacts)
   }
+
+  const closeModal = () => {
+    setIsModalOpen(false)
+  }
+
+  const openCreateModal = () => {
+    if(!isModalOpen){
+      setIsModalOpen(true)
+    }
+  }
+
   return (
-   <ContactList contacts={contacts} />
+    <>
+      <ContactList contacts={contacts} />
+      <button onClick={openCreateModal}>Create Contact</button>
+      {
+        isModalOpen && (
+          <div className="modal">
+            <div className="modal-content">
+              <span className="close" onClick={closeModal}>&times;</span>
+              <ContactForm fetchContacts={fetchContacts} />
+            </div>
+          </div>
+        )
+      }
+    </>
   )
 }
 
